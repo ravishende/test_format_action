10ebc7e07c575a9348bf7b9e32e476b3cd6e2d01
ravishende <ravi.sameer.shende@gmail.com>
2025-09-17 18:24:10 -0700

    added unformatted code to test if the autoPR works

---
+++ test_file.py
@@ -0,0 +1,9 @@
+if __name__ == "__main__":
+    x = 5
+    y = x + \
+        5
+    z = x + y
+    sum([x, y,
+        z])
+    # this is an extra long comment that should hopefully be split across multiple lines if black formatting is correctly applied. If it is not, this comment will stay on one line, despite it being far more than the usual characters per line.
+
